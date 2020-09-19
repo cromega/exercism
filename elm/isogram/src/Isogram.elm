5d198916872c6b7e12e6686f10bb4df9ad2a55ea
Bence Monus <crome@moronia.hu>
2020-09-19 23:30:08 +0200

    count distinct elements properly

--- elm/isogram/src/Isogram.elm
+++ elm/isogram/src/Isogram.elm
@@ -8,8 +8,19 @@ isIsogram sentence =
     letters = String.toLower sentence |> String.toList |> List.filter Char.isAlpha
   in
 
-  List.length letters == uniqueCount letters
+  (List.length <| uniq letters) == List.length letters
 
-uniqueCount : List comparable -> Int
-uniqueCount list =
-  Set.size <| Set.fromList list
+uniq : List comparable -> List comparable
+uniq list =
+  List.sort list
+  |> List.foldr keepIfUnique []
+
+keepIfUnique : a -> List a -> List a
+keepIfUnique val acc =
+  case acc of
+    [] -> val :: acc
+    x :: _ ->
+      if x == val then
+        acc
+      else
+        val :: acc
